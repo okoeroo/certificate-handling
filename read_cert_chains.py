c399169c5715a7989a7a50b5667ca36fe2acb16a
Oscar Koeroo <okoeroo@gmail.com>
2011-08-01 19:58:34 +0200

    Added more beautifications

--- read_cert_chains.py
+++ read_cert_chains.py
@@ -25,18 +25,20 @@ from OpenSSL.crypto import NetscapeSPKI, NetscapeSPKIType
 # os.path.split
 
 class CertificatePathFinder(object):
-    def __init__(self, cadir):
+    def __init__(self, cadir, match=".pem"):
         if cadir == None and cadir != '':
             print "No CA directory given to print"
             raise
 
         self.ca_certificates = []
+        self.use_match = match
 
         self.load_ca_certificates(cadir)
 
     def load_ca_certificates(self, cadir='/etc/grid-security/certificates'):
         for i in os.listdir(cadir):
-            if i.endswith('.0'):
+            if i.endswith(self.use_match):
+            #if i.endswith('.0'):
                 f = open(cadir + '/' + i)
                 b = f.read()
 
@@ -71,10 +73,22 @@ class CertificatePathFinder(object):
         ca = ca_d['cert']
         if indent == 0:
             print "%s \"%s\"" % (self.indenter(indent), self.printAsOpenSSLoneline(ca.get_subject()))
-            print "%s     |     \->    File : %s, Depth: %d, Not Before: %s, Not After %s" % (self.indenter(indent), ca_d['file'], indent, ca.get_notBefore(), ca.get_notAfter())
         else:
             print "%s \___| -> \"%s\"" % (self.indenter(indent), self.printAsOpenSSLoneline(ca.get_subject()))
-            print "%s     |     \->    File : %s, Depth: %d, Not Before: %s, Not After %s" % (self.indenter(indent), ca_d['file'], indent, ca.get_notBefore(), ca.get_notAfter())
+        #print "%s     |     |->    File : %s, Depth: %d, Not Before: %s, Not After %s" % (self.indenter(indent), ca_d['file'], indent, ca.get_notBefore(), ca.get_notAfter())
+
+        print "%s     |   |-> File             : %s" % (self.indenter(indent), ca_d['file'])
+        print "%s     |   |-> Depth            : %d" % (self.indenter(indent), indent)
+        print "%s     |   |-> Not Before       : %s" % (self.indenter(indent), ca.get_notBefore())
+        print "%s     |   |-> Not After        : %s" % (self.indenter(indent), ca.get_notAfter())
+        print "%s     |   |-> Serial number    : %d" % (self.indenter(indent), ca.get_serial_number())
+        if ca.has_expired():
+            print "%s     |   |-> Is valid         : YES" % (self.indenter(indent))
+        else:
+            print "%s     |   |-> Is valid         : no" % (self.indenter(indent))
+
+        print "%s     |   #" % (self.indenter(indent))
+
 #        print "CA depth %d : %s%s in file: %s" % (indent, self.indenter(indent), str(ca.get_issuer().get_components()),  ca_d['file'])
 
         for d in ca_list:
@@ -110,5 +124,6 @@ class CertificatePathFinder(object):
 #            print x508Name.get_components()
 
 
-m = CertificatePathFinder('/etc/grid-security/certificates')
+m = CertificatePathFinder('/etc/grid-security/certificates', '.pem')
 m.showCertificates()
+
